0b68a5edec32f84818c5350af53568b6c1d62e4e
NnQqDd <Quangduy162003@gmail.com>
2026-07-07 05:48:45 +0000

    Update

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="nqd-audiomentations",
-    version="1.1.5",
+    version="1.1.7",
     packages=find_packages(),
     install_requires=[
         "numpy",
         "audiomentations",
-        "pyrubberband==0.4.0",
         "pyroomacoustics==0.10.0",
         "scipy",
         "librosa"
